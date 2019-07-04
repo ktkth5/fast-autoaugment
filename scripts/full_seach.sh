4ecfd905e86d109b96a2a27143cd241abf27f25d
ktkth5 <zcdjkjfygxnc9w88mmt8@gmail.com>
2019-07-04 17:00:35 +0900

    cp

--- scripts/full_seach.sh
+++ scripts/full_seach.sh
@@ -1,3 +1,3 @@
 # done on aws p3.xlarge
-python3 main.py --B=200 --T=2 --num_samples=100 --max_concurrent=10 --epochs=100 --batch_size=64 --cpu=8 --gpu=1 --aws
+python3 main.py --B=50 --T=2 --num_samples=50 --max_concurrent=5 --epochs=100 --batch_size=64 --cpu=8 --gpu=1 --aws
 python3 bestparams.py --exp_dir=/home/ubuntu/ray_results/TrainCIFAR --K=10 --log_dir=checkpoint
